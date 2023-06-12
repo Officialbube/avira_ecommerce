bfbcc35df6449c38dfc0c9793d9a448a9c2c03e6
officialbube <ebubenkereebube@gmail.com>
2023-06-12 21:54:54 +0100

    imported ToastContainer toast from react-toastify

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Header, Footer} from './components'
 import { HomePage, Shop, About, Login, Register, Reset } from './pages';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import './App.css';
 
@@ -13,6 +14,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
+      <ToastContainer />
       <Header />
         <Routes>
           <Route path='/' element={ <HomePage/>} />
