21ea7ff98fa6556656c0d49c6e2fb4228ed102df
officialbube <ebubenkereebube@gmail.com>
2023-06-16 21:22:32 +0100

    changed the reset file name from Reset.jsx to Reset.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,5 @@ export {default as Shop} from './shop/Shop.jsx';
 export {default as About} from './about/About.jsx';
 export {default as Login} from './auth/Login.jsx';
 export {default as Register} from './auth/Register.js';
-export {default as Reset} from './auth/Reset.jsx';
+export {default as Reset} from './auth/Reset.js';
 export {default as Contact} from './contact/Contact.jsx'
